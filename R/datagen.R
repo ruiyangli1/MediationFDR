851a09819b3fb4d66b5419e32d426111e26deb32
Ruiyang Li <rl3034@cumc.columbia.edu>
2022-04-12 22:20:25 -0400

    fix notation

--- R/datagen.R
+++ R/datagen.R
@@ -4,7 +4,7 @@
 #' @description For each subject i = 1, ..., n:
 #' - \eqn{X_i ~ i.i.d Bernoulli(p = 0.5)}
 #' - \eqn{M_{i,k} = \alpha_k X_i + e_{1_{i,k}}}, where \eqn{e_{1_{i,k}} ~ i.i.d N(0, 1), k = 1, ..., K}
-#' - \eqn{Y_i = X_i + \sum_k b_{i,k} M_{i,k} + e_{2_{i,k}}}, where \eqn{e_{2_{i,k}} ~ i.i.d. N(0, 1)}
+#' - \eqn{Y_i = X_i + \sum_k b_{k} M_{i,k} + e_{2_{i}}}, where \eqn{e_{2_{i}} ~ i.i.d. N(0, 1)}
 #' @description The 6th-10th M variables (M6,M7,M8,M9,M10) are set to be the true mediators (i.e., having non-zero alpha and beta coefficients).
 #' @description Specifically, the first 10 coefficients of alpha are set to take some non-zero value a, the 6th to 15th coefficients of beta are set to take some non-zero value b, and all other alpha and beta coefficients are set to be 0.
 #'
