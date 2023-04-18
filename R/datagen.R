9850b6e03492f6577f85480161c913597305c470
Ruiyang Li <rl3034@cumc.columbia.edu>
2023-04-17 22:49:03 -0400

    fix notation in datagen()

--- R/datagen.R
+++ R/datagen.R
@@ -3,22 +3,23 @@
 #' @description This function generates the simulation data (exposure X, outcome Y, and potential mediators M).
 #' @description For each subject i = 1, ..., n:
 #' - \eqn{X_i \stackrel{i.i.d}{\sim} Bernoulli(prob = 0.5)}
-#' - \eqn{M_{i,k} = \alpha_k X_i + e_{1_{i,k}}}, where \eqn{e_{1_{i,k}} \stackrel{i.i.d}{\sim} N(0, \Sigma), k = 1, ..., p, \Sigma} is the variance-covariance matrix with diagonal entries 1 and off-diagonal entries rho
+#' - \eqn{M_{i,k} = \alpha_k X_i + e_{1_{i,k}}}, where \eqn{e_{1_{i,k}} \stackrel{i.i.d}{\sim} N(0, 1), k = 1, ..., p}, and \eqn{cov(e_{1_{i,k}}, e_{1_{i,j}}) = \rho} for k not equal j
 #' - \eqn{Y_i = X_i + \sum_k b_{k} M_{i,k} + e_{2_{i}}}, where \eqn{e_{2_{i}} \stackrel{i.i.d}{\sim} N(0, 1)}
-#' @description The 6th-15th M variables (M6,M7,M8,M9,M10,M11,M12,M13,M14,M15) are set to be the true mediators (i.e., having non-zero alpha and beta coefficients).
+#' @description The 6th-15th M variables (i.e., M6,M7,...,M14,M15) are set to be the true mediators (i.e., having non-zero alpha and beta coefficients).
 #' @description Specifically, the first 15 coefficients of alpha are set to take some non-zero value a, the 6th to 20th coefficients of beta are set to take some non-zero value b, and all other alpha and beta coefficients are set to be 0.
 #'
 #' @param n sample size
 #' @param p number of potential mediators
-#' @param a effect size for X-M path (i.e., non-zero alpha coefficient)
-#' @param b effect size for M-Y path (i.e., non-zero beta coefficient)
-#' @param rho effect size for the correlation structure of M
+#' @param a effect size of X-M path (i.e., non-zero alpha coefficient)
+#' @param b effect size of M-Y path (i.e., non-zero beta coefficient)
+#' @param rho effect size of the correlation structure of M
 #'
 #' @return The resulting data has 3 lists: X, Y and M
 #' @export
 #'
 #' @examples
-#' set.seed(20205); data = datagen(n = 1000, p = 100, a = 0.5, b = 0.3, rho = 0.1)
+#' set.seed(20205)
+#' data = datagen(n = 1000, p = 100, a = 0.5, b = 0.3, rho = 0.1)
 #' head(data$X)
 #' head(data$Y)
 #' head(data$M)
